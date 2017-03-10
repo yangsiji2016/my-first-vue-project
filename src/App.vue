b1aecee0ca7b0a84eab3606f44eb4badced56f2b
yangsiji2016 <yangsiji2016@gmail.com>
2017-03-10 15:08:31 +0800

    111

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <h1 v-html="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
+    <p>child tells me: {{ childWords }}</p>
+    <component-a msgfromfather="you die!1123444" v-on:child-tell-me-something="listenToMyBoy"></component-a>
+  </div>
+</template>
+
+<script>
+  import Store from './store'
+  import ComponentA from './components/componentA'
+  export default {
+    data: function () {
+      return {
+        title: 'this is a todo list',
+        items: Store.fetch(),
+        newItem: '',
+        childWords: ''
+      }
+    },
+    components: { ComponentA },
+    watch: {
+      items: {
+        handler: function (items) {
+          Store.save(items)
+        },
+        deep: true
+      }
+    },
+    methods: {
+      toggleFinish: function (item) {
+        item.isFinished = !item.isFinished
+      },
+      addNew: function () {
+        this.items.push({
+          label: this.newItem,
+          isFinished: false
+        })
+        this.newItem = ''
+      },
+      listenToMyBoy: function (msg) {
+        this.childWords = msg;
+      }
+    }
+  }
+</script>
+
+<style>
+  .finished{
+    text-decoration: line-through;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  #app {
+    color: #2c3e50;
+    margin-top: -100px;
+    max-width: 600px;
+    font-family: Source Sans Pro, Helvetica, sans-serif;
+    text-align: center;
+  }
+
+  #app a {
+    color: #42b983;
+    text-decoration: none;
+  }
+
+  .logo {
+    width: 100px;
+    height: 100px
+  }
+</style>
